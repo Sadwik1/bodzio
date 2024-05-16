cdafb017d26d18fc7f4700e4c65ff29211111b19
Sadwik1 <114020422+Sadwik1@users.noreply.github.com>
2024-05-17 00:45:09 +0200

    Add files via upload
    jakie walone 7h pracy

---
+++ index.js
@@ -0,0 +1,12 @@
+document.getElementById("sit1").onclick = function () {
+    location.href = "site1";
+};
+document.getElementById("sit2").onclick = function () {
+    location.href = "site2";
+};
+document.getElementById("sit3").onclick = function () {
+    location.href = "site3";
+};
+document.getElementById("sit4").onclick = function () {
+    location.href = "site4";
+};
